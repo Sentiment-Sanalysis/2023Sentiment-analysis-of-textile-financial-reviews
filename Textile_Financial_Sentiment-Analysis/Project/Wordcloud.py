3676eb3ac3c3fae85c5a47e483c7787f0777eb90
‘wangzihan’ <2018952617@qq.com>
2023-03-09 15:10:00 +0800

    The project structure has been adjusted

---
+++ Textile_Financial_Sentiment-Analysis/Project/Wordcloud.py
@@ -0,0 +1,58 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   Wordcloud.py
+@Time    :   2023/03/05 22:55:38
+@Author  :   ZihanWang
+'''
+
+import jieba
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+def word_cloud(filepath,outpass):
+
+    # 打开文本文件，读取内容
+    with open(filepath, 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    # 使用jieba对文本进行分词
+    words = jieba.cut(text)
+
+    # 将分词结果转换为空格分隔的字符串
+    words_str = ' '.join(words)
+
+    #停词
+    std = open('./stopwords.txt',encoding="utf-8").read()
+
+    # 生成词云
+    wc = WordCloud(
+        # 参数列表
+        font_path='./HGKT_CNKI.TTF',  # 设置字体
+        background_color='white',  # 设置背景颜色
+        width=800,  # 设置宽度
+        height=600,  # 设置高度
+        max_words=200,  # 设置最大词数
+        max_font_size=100,  # 设置最大字体大小
+        stopwords=std # 设置停用词
+    )
+
+    # 生成词云图片
+    wc.generate(words_str)
+    # 显示词云图片
+    plt.imshow(wc)
+    plt.axis('off')
+    # 返回词云图片plt对象,module类型
+    # 可以使用plt.show()显示图片
+    # 也可以使用plt.savefig()保存图片
+    plt.savefig(outpass)
+    return plt
+    # plt.show()
+
+
+if __name__ == '__main__':
+    filepath = './test.txt'
+    plt = word_cloud(filepath,'./wc.png')
+    plt.show()
+
+
--- 情感分析/.history/Main_20230304221644.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, make_response, request # 注意需导入 make_response
-
-app = Flask(__name__)
-
-@app.route("/set_cookies")
-def set_cookie():
-    resp = make_response("success")
-    resp.set_cookie("w3cshool", "w3cshool",max_age=3600<span style="background-color: initial; font-size: inherit;">)</span>
